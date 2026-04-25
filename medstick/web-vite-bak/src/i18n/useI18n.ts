81c29a8944e7ccd37695d05cf7a5fff9d08e63da
ahmed-52 <aaa384@cornell.edu>
2026-04-25 16:41:52 -0400

    clean up

--- medstick/web-vite-bak/src/i18n/useI18n.ts
+++
@@ -1,7 +0,0 @@
-import { STRINGS } from './strings'
-import { useApp } from '../store/app'
-
-export function useI18n() {
-  const lang = useApp((s) => s.lang)
-  return { t: STRINGS[lang], lang, isRtl: lang === 'ar' }
-}
